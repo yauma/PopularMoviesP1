e03fb14d756be2064f987842591572f8e42e756a
Jaime <jqueraltsound@gmail.com>
2015-09-07 21:08:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jaimequeralt/popularmovies/ImageAdapter.java
@@ -0,0 +1,92 @@
+package com.example.jaimequeralt.popularmovies;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import static android.widget.ImageView.ScaleType.CENTER_CROP;
+
+/**
+ * Created by jaimequeralt on 9/6/15.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        SquaredImageView view = (SquaredImageView) convertView;
+        if (view == null) {
+            view = new SquaredImageView(mContext);
+            view.setScaleType(CENTER_CROP);
+
+        }
+
+        // Trigger the download of the URL asynchronously into the image view.
+        Picasso.with(mContext)
+                .load(mThumbIds[position])
+                .resize(300, 300)
+                .centerCrop()
+                .into(view);
+
+        return view;
+    }
+}
+
+/**
+ * An image view which always remains square with respect to its width.
+ */
+final class SquaredImageView extends ImageView {
+    public SquaredImageView(Context context) {
+        super(context);
+    }
+
+    public SquaredImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}
+
---
+++ app/src/main/java/com/example/jaimequeralt/popularmovies/MainActivityFragment.java
@@ -0,0 +1,39 @@
+package com.example.jaimequeralt.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_gridview, container, false);
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getActivity().getApplicationContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "Image" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+
+}
